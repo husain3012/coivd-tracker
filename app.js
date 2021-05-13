f9f66e5f0df0dfcbdbfae6eedba90418b3044d8b
Husain <hsr3012kalimg@gmail.com>
2021-05-13 19:12:57 +0530

    add procfile

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started on port 3000");
 });
